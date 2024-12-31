b4dcc03124c0ffb857f736369cd9dbf194069112
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2024-12-31 21:47:17 +0530

    1. even odd check by boolean function.

---
+++ odd_even.java
@@ -0,0 +1,21 @@
+//using function  return true for even and false for odd
+import java.util.*;
+
+public class odd_even{
+    public boolean check(int n){
+        if(n% 2 == 0){
+            return true;
+        }else{
+            return false;
+        }
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter ");
+        int num = scanner.nextInt();
+
+        odd_even obj = new odd_even();
+        boolean v = obj.check(num);
+        System.out.println(v);
+    }
+}
