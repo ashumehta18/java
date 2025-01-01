b7e2c977f82aac2a5c7912a95f967a3675286674
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-01-01 16:45:04 +0530

    swaping no.s without third varriable.

---
+++ swap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class swap{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        n = n^m;
+        m = m^n;
+        n = n^m;
+        System.out.println(n + " and " + m);
+    }
+}
