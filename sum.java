1064a420f3c13101a39969ff849dd8c955260d8a
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-01-01 18:08:30 +0530

    addition of no. entered by user

---
+++ sum.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class sum{
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int n = scanner.nextInt();
+    int m = scanner.nextInt();
+
+    int sum = n+m;
+    System.out.println(n +" + "+ m + " = " + sum);
+    }
+}
