0659e1802aceb37dbd9ca548a20f7c3fa741dcb8
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-01-07 12:31:36 +0530

    Update String_match.java

--- String_match.java
+++ String_match.java
@@ -26,3 +26,22 @@ Constraints:
 1 <= words[i].length <= 30
 words[i] contains only lowercase English letters.
 All the strings of words are unique.
+
+
+ solution:-
+ class Solution {
+    public List<String> stringMatching(String[] words) {
+       List<String> result = new ArrayList<>(); 
+       for (int i = 0; i < words.length; i++) 
+       { 
+        for (int j = 0; j < words.length; j++) 
+        { 
+            if (i != j && words[j].contains(words[i]) && !result.contains(words[i])) 
+            { 
+                result.add(words[i]);
+             }
+         }
+        }
+        return result;
+    }
+}
