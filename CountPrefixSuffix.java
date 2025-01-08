44f24655140fbf24c01d5e4446023a7f6826e1a9
ASHU MEHTA <158246970+ashumehta18@users.noreply.github.com>
2025-01-08 07:57:41 +0530

    Update CountPrefixSuffix.java

--- CountPrefixSuffix.java
+++ CountPrefixSuffix.java
@@ -34,3 +34,24 @@ Constraints:
 1 <= words.length <= 50
 1 <= words[i].length <= 10
 words[i] consists only of lowercase English letters.
+
+
+ SOLUTION:-
+ class Solution {
+    public int countPrefixSuffixPairs(String[] words) {
+        int count = 0;
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i + 1; j < words.length; j++) {
+                if (isPrefixAndSuffix(words[i], words[j])) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    private boolean isPrefixAndSuffix(String str1, String str2) {
+        return str2.startsWith(str1) && str2.endsWith(str1);
+    }
+}
+
